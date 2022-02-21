19a76cca567c35e86ca92ad7767dfa030761fd05
gorbit99 <gorbitgames@gmail.com>
2022-02-21 14:56:38 +0100

    Delete unneeded steps variable

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -5,7 +5,6 @@ import hu.bme.mit.train.interfaces.TrainController;
 public class TrainControllerImpl implements TrainController {
 
     //Steps
-	private int step = 0;
     //Reference Speed
 	private int referenceSpeed = 0;
     //Speed Limit
